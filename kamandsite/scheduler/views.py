d9feb1e6bcb6801f095752a4ac3f09e09f0a3f29
bryancsmith2424 <bryancsmith2424@yahoo.com>
2020-08-13 11:39:00 -0400

    Added form for users to add assignments they have coming up

--- kamandsite/scheduler/views.py
+++ kamandsite/scheduler/views.py
@@ -4,21 +4,26 @@ from django.urls import reverse
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 
 
 def index(request):
     return render(request, 'scheduler/index.html')
 
+@login_required(login_url='/scheduler/')
 def start(request, user_id):
     user = User.objects.get(pk=user_id)
     context = {'user': user}
     return render(request, 'scheduler/start.html', context)
 
+@login_required(login_url='/scheduler/')
 def goals(request, user_id):
     user = User.objects.get(pk=user_id)
     context = {'user': user, 'user_id': user_id}
     return render(request, 'scheduler/goals.html', context)
 
+@login_required(login_url='/scheduler/')
 def update_goals(request, user_id):
     user = User.objects.get(pk=user_id)
     try:
